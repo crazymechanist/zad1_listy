e6ca8f4db96059a1355f9e8ae00d4eff6747cf8f
Mateusz Nowakowski <mateusz.nowakowski.programista@gmail.com>
2021-07-08 00:27:17 +0200

    Appying change from @

--- src/datastructure.list/CustomArrayList.java
+++ src/datastructure.list/CustomArrayList.java
@@ -1,75 +1,12 @@
 package datastructure.list;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 
+import java.util.List;
 
 public class CustomArrayList<E> implements List<E>{
 
     @Override
-    public int size() {
-        return 0;
-    }
+    public void add(int index, E element) {
 
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean add(E e) {
-        return false;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
     }
 
     @Override
@@ -78,22 +15,12 @@ public class CustomArrayList<E> implements List<E>{
     }
 
     @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
     public E get(int index) {
-        return null;
-    }
-
-    @Override
-    public E set(int index, E element) {
-        return null;
-    }
-
-    @Override
-    public void add(int index, E element) {
-
-    }
-
-    @Override
-    public E remove(int index) {
         return null;
     }
 
@@ -103,23 +30,23 @@ public class CustomArrayList<E> implements List<E>{
     }
 
     @Override
-    public int lastIndexOf(Object o) {
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public int size() {
         return 0;
-    }
-
-    @Override
-    public ListIterator<E> listIterator() {
-        return null;
-    }
-
-    @Override
-    public ListIterator<E> listIterator(int index) {
-        return null;
-    }
-
-    @Override
-    public List<E> subList(int fromIndex, int toIndex) {
-        return null;
     }
 }
 
--- src/datastructure.list/CustomLinkedList.java
+++ src/datastructure.list/CustomLinkedList.java
@@ -1,71 +1,12 @@
 package datastructure.list;
-import java.util.*;
+
+import java.util.List;
 
 public class CustomLinkedList<E> implements java.util.List<E>{
 
     @Override
-    public int size() {
-        return 0;
-    }
+    public void push(int index, E element) {
 
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean add(E e) {
-        return false;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
     }
 
     @Override
@@ -74,22 +15,12 @@ public class CustomLinkedList<E> implements java.util.List<E>{
     }
 
     @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
     public E get(int index) {
-        return null;
-    }
-
-    @Override
-    public E set(int index, E element) {
-        return null;
-    }
-
-    @Override
-    public void add(int index, E element) {
-
-    }
-
-    @Override
-    public E remove(int index) {
         return null;
     }
 
@@ -99,22 +30,23 @@ public class CustomLinkedList<E> implements java.util.List<E>{
     }
 
     @Override
-    public int lastIndexOf(Object o) {
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public E pop(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public int size() {
         return 0;
     }
-
-    @Override
-    public ListIterator<E> listIterator() {
-        return null;
-    }
-
-    @Override
-    public ListIterator<E> listIterator(int index) {
-        return null;
-    }
-
-    @Override
-    public List<E> subList(int fromIndex, int toIndex) {
-        return null;
-    }
 }
+
